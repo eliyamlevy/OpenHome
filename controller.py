e3cc50efe92ec1261888105612343e4594cb2667
Eliyam Levy <eliyamle@usc.edu>
2021-02-22 16:43:27 -0500

    added controller file and folder for sound effects

---
+++ controller.py
@@ -0,0 +1,11 @@
+from HardwareInterface import HardwareInterface
+
+if __name__ == '__main__':
+    #sound effect list
+    smap = {
+        1 : "sounds/piano.wav"
+    }
+    #hardware interface instantiation
+    hwi = HardwareInterface(smap)
+
+    
