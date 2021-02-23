291cc4dc1a43c6bf1a362185ef4d64d08a8d811c
Eliyam Levy <eliyamle@usc.edu>
2021-02-22 19:42:57 -0500

    satrtup scripts

--- event_queue/startup.sh
+++ event_queue/startup.sh
@@ -4,12 +4,12 @@ cd nsq/build
 
 echo "Starting lookup"
 
-./nsqlookupd 
+./nsqlookupd &
 
 echo "Starting admin"
 
-./nsqadmin --lookupd-http-address localhost:4161
+./nsqadmin --lookupd-http-address localhost:4161 &
 
 echo "Starting nsq"
 
-./nsqd -lookupd-tcp-address localhost:4160
+./nsqd -lookupd-tcp-address localhost:4160 & 
