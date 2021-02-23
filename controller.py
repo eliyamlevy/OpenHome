eccdbd4b0a1ff8444d90ad07f296176914ae587e
Cole Schroeder <coleschr@usc.edu>
2021-02-23 15:15:29 -0800

    Changed ip to loopback

--- controller.py
+++ controller.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     #Reader instantiation
     r = nsq.Reader(message_handler=handler,
-    lookupd_http_addresses=['http://192.168.80.30:4161'],
+    lookupd_http_addresses=['http://127.0.0.1:4161'],
     topic='test', channel='controller', lookupd_poll_interval=15)
 
     nsq.run()
