53e3550b6c90893822a0a4f9348b2fda412110e8
coleschr <coleschr@usc.edu>
2021-03-06 12:03:42 -0800

    clock parsing update and err

--- widgets/clock.py
+++ widgets/clock.py
@@ -9,22 +9,33 @@ functions = {"get_time": get_time,
              "get_date": get_date}
 
 def get_time():
-    curr_time = time.ctime().strftime('%I:%M %p').replace(' ', '_')
-    requests.post('http://127.0.0.1:4151/pub?topic=test',data='xxresp clock time '+curr_time+'x')
+    curr_time = time.ctime().strftime('%I:%M %p')
+    respond("time", [curr_time])
 
 def get_time_in_loc(location):
-    curr_time = time.ctime().strftime('%I:%M %p').replace(' ', '_')
-    requests.post('http://127.0.0.1:4151/pub?topic=test', data='xxresp clock time ' + curr_time + 'x')
+    curr_time = time.ctime().strftime('%I:%M %p')
+    respond("time", [curr_time])
 
 def get_date():
-    curr_date = time.ctime().strftime('%a %b %d %Y').replace(' ', '_')
-    requests.post('http://127.0.0.1:4151/pub?topic=test', data='xxresp clock date ' + curr_date + 'x')
+    curr_date = time.ctime().strftime('%a %b %d %Y')
+    respond("date", [curr_date])
+
+def respond(title, args):
+    strings = ["resp", "clock", title]
+    strings.append(args)
+    resp = '&'.join(strings)
+    requests.post('http://127.0.0.1:4151/pub?topic=test', data='xx' + resp + 'x')
+
+def error(message):
+    strings = ["resp", "clock", "err", message]
+    resp = '&'.join(strings)
+    requests.post('http://127.0.0.1:4151/pub?topic=test', data='xx' + resp + 'x')
 
 def handler(message):
     print(message.id)
     print(message.body)
 
-    msgSplit = str(message.body).split(" ")
+    msgSplit = str(message.body).split("&")
     print(msgSplit)
     if msgSplit[0] == "cmd":        #incoming command from controller
         args = ()
