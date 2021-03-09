896cc533a0b292996d5bf0db154b37e7d1f57ffb
Eliyam Levy <eliyamle@usc.edu>
2021-03-09 16:20:22 -0500

    added tts and sound playing

--- HardwareInterface.py
+++ HardwareInterface.py
@@ -1,3 +1,6 @@
+from gtts import gTTS 
+import os
+
 class HardwareInterface:
 
     #sound effects should be a dictionary mapping ints to filepaths
@@ -8,10 +11,27 @@ class HardwareInterface:
     #sound will be an int referencing a preset list of effects
     def playSound(self, sound):
         print("Playing sound " + str(sound))
+        # Playing the converted file 
+        os.system("mpg321 " + self.sound_effects[sound])
 
     #does text to speak on an inputted text
     def speak(self, text):
         print("Speaker says: " + text)
+        language = 'en'
+
+        # Passing the text and language to the engine, 
+        # here we have marked slow=False. Which tells 
+        # the module that the converted audio should 
+        # have a high speed 
+        myobj = gTTS(text=text, lang=language, slow=False) 
+
+        # Saving the converted audio in a mp3 file named 
+        # welcome 
+        myobj.save("sounds/say.mp3") 
+
+        # Playing the converted file 
+        os.system("mpg321 sounds/say.mp3") 
+
 
     #does the led controls
     def flash(self, color):
