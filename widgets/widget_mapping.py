b96300e8da04a37d9fbe9e54a99173c305b9786e
coleschr <coleschr@usc.edu>
2021-03-30 15:24:30 -0700

    intent to widget mapping

---
+++ widgets/widget_mapping.py
@@ -0,0 +1,26 @@
+function_to_widget = {'incr_volume': 'util',
+                      'decr_volume': 'util',
+
+                      'get_time': 'clock',
+                      'get_time_in_loc': 'clock',
+                      'get_date': 'clock',
+
+                      'set_alarm': 'alarm',
+                      'stop_alarm': 'alarm',
+                      'snooze_alarm': 'alarm',
+                      'cancel_alarm': 'alarm',
+
+                      'get_weather': 'weather',
+                      'get_weather_in_loc': 'weather',
+
+                      'play': 'spotify',
+                      'pause': 'spotify',
+                      'skip': 'spotify',
+                      'rewind': 'spotify',
+
+                      'change_light_state': 'hue',
+                      'change_brightness': 'hue',
+                      'dim': 'hue',
+                      'brighten': 'hue',
+                      'change_color': 'hue',
+                      }
