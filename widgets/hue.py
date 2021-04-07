067edc93ba9e498bbff8cb80112f676799c5ed49
jonahp99 <jeperetz@usc.edu>
2021-04-07 13:00:07 -0900

    all light functionality implemented

--- widgets/hue.py
+++ widgets/hue.py
@@ -1,8 +1,9 @@
 import paho.mqtt.client as mqtt
 import time
 from switch import switch
+import json
 
-s = switch()
+s = None
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe("openhome/hue")
@@ -24,32 +25,63 @@ def error(message):
     client.publish("openhome/controller", resp)
 
 def on():
+    if s is None:
+        error("Error: no connected lights or bridge found")
+        return
     s.on()
     respond(["Lights turned on"])
 
 def off():
+    if s is None:
+        error("Error: no connected lights or bridge found")
+        return
     s.off()
     respond(["Lights turned off"])
 
 def brighten():
+    if s is None:
+        error("Error: no connected lights or bridge found")
+        return
     s.brighten()
     respond(["Lights brightened"])
 
 def dim():
+    if s is None:
+        error("Error: no connected lights or bridge found")
+        return
     s.dim()
     respond(["Lights dimmed"])
 
 def set_color(color):
+    if s is None:
+        error("Error: no connected lights or bridge found")
+        return
     colors = ['white', 'red', 'blue', 'green']
-    s.set_color(color)
-    respond(["Hue set to color: " + colors[color]])
+    s.set_color(colors.index(color))
+    respond(["Lights set to " + color])
+
+def bridge_connect(ip_address):
+    global s
+    if ip_address is not None:
+        write_data = {"ip_address" : str(ip_address)}
+        s = switch(ip_address)
+    else:
+        with open('widgets/configs/hue.json') as hue_config:
+            read_data = json.load(hue_config)
+            if 'ip_address' in read_data and read_data['ip_address'] is not None:
+                ip_address = read_data['ip_address']
+                # write_data = {"ip_address" : str(ip_address)}
+                s = switch(ip_address)
+                # print("switch instantiated:", s)
+                # print("ip_address found in configs:", ip_address)
 
 functions = {
                 "on": on,
                 "off": off,
                 "brighten": brighten,
                 "dim": dim,
-                "set_color": set_color
+                "set_color": set_color,
+                "bridge_connect": bridge_connect
             }
 
 def handler(client, userdata, msg):
@@ -70,6 +102,7 @@ if __name__ == '__main__':
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = handler
+    bridge_connect(None) # will only connect if ip_address exists in configs, otherwise wait for ip_address
 
     client.connect("localhost", 1883)
     client.loop_forever()
--- widgets/switch.py
+++ widgets/switch.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python
 
 from phue import Bridge
-# import logging
-# logging.basicConfig()
+import time as t
 
 class switch:
 
-    def __init__(self):
-        self.b = Bridge('10.0.0.122')
+    def __init__(self, ip_address):
+        self.b = Bridge(ip_address)
+        self.b.connect()
+        api = self.b.get_api()
+        self.lights = list() # contains ids of all the lights associated with the bridge
+        if api is not None:
+            for light in api['lights']:
+                self.lights.append(int(light[0]))
+                print(self.lights)
         self.set_color()
         self.off()
 
-
     def on(self):
-        self.b.set_light(3, 'on', True)
+        for light in self.lights:
+            self.b.set_light(light, 'on', True)
+            self.b.set_light(light, 'xy', [0.4835, 0.4144])
+            self.b.set_light(light, 'bri', 254)
 
     def off(self):
-        self.b.set_light(3, 'on', False)
+        for light in self.lights:
+            self.b.set_light(light, 'on', False)
 
     def brighten(self):
-        self.b.set_light(3, 'bri', 254) # 254 = max lumens
+        api = self.b.get_api()
+        curr_lumens = api['lights'][str(self.lights[0])]['state']['bri']
+        curr_lumens = int(curr_lumens)
+        for light in self.lights:
+            self.b.set_light(light, 'bri', min(254, curr_lumens + 100)) # 254 = max lumens
 
     def dim(self):
-        self.b.set_light(3, 'bri', 100)
+        api = self.b.get_api()
+        curr_lumens = api['lights'][str(self.lights[0])]['state']['bri']
+        curr_lumens = int(curr_lumens)
+        for light in self.lights:
+            self.b.set_light(light, 'bri', max(0, curr_lumens - 100)) # 254 = max lumens
 
     def set_color(self, color=0):
         '''
@@ -43,5 +60,18 @@ class switch:
         elif color == 3:
             # green
             xy = [0.3, 0.65]
-        
-        self.b.set_light(3, 'xy', xy)
+        for light in self.lights:
+            self.b.set_light(light, 'xy', xy)
+
+# s = switch('10.0.0.122')
+# s.on()
+# t.sleep(2)
+# s.dim()
+# t.sleep(2)
+# s.dim()
+# t.sleep(2)
+# s.dim()
+# t.sleep(2)
+# s.brighten()
+# t.sleep(2)
+# s.on()
