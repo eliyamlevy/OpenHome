4cb698bfadf196b8a82541295c89d9becac352b4
Eliyam Levy <eliyamle@usc.edu>
2021-04-07 16:02:31 -0400

    editted webpages

--- web/hello.py
+++ web/hello.py
@@ -2,53 +2,129 @@ from bottle import run, get, post, request
 import paho.mqtt.publish as publish
 import json
 
-#Test page
-@get('/') # or @route('/login')
+#Splash page
+@get('/')
 def login_form():
     return '''  <!doctype html>
 
                 <html lang="en">
-                <head>
-                <meta charset="utf-8">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>OpenHome</title>
+                        <meta name="description" content="The web interface for your OpenHome!">
+                        <meta name="author" content="OpenHome">
+                        <link rel="stylesheet" href="css/styles.css?v=1.0">
+                    </head>
 
-                <title>OpenHome</title>
-                <meta name="description" content="The web interface for your OpenHome!">
-                <meta name="author" content="OpenHome">
+                    <body>
+                        <script src="index.js"></script>
+                        <div class="body">
+                            <div class="header">
+                                <h2> Welcome to OpenHome </h2>
+                            </div>
+                            <div class="forms">
+                                <form method="POST" action="/">
+                                    <input id="terminalInput" type="text" name="terminalInput" placeholder="Input commands here ..."/> <br>
+                                    <input id="publish" type="submit" value="publish">
+                                </form>'
+                            </div>
+                        </div>
+                    </body>
+                </html> '''
 
-                <link rel="stylesheet" href="css/styles.css?v=1.0">
+@post('/')
+def login_form():
+    cmd = request.forms.get('terminalInput')
+    print(cmd)
+    #publish.single("openhome/controller", cmd)
+    return '''  <!doctype html>
 
-                </head>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>OpenHome</title>
+                        <meta name="description" content="The web interface for your OpenHome!">
+                        <meta name="author" content="OpenHome">
+                        <link rel="stylesheet" href="css/styles.css?v=1.0">
+                    </head>
 
-                <body>
-                <script src="index.js"></script>
-                <div class="body">
-                    Welcome to OpenHome <br />
-                    <div class="forms">
-                        <form name="terminal">
-                            <input id="terminalInput" type="text" name="terminalInput" placeholder="Input commands here ..."/>
-                            <input id="publish" type="submit" value="publish" onclick="return publishCMD()">
-                        </form>
-                    </div>
-                </div>
-                </body>
+                    <body>
+                        <script src="index.js"></script>
+                        <div class="body">
+                            <div class="header">
+                                <h2> Welcome to OpenHome </h2>
+                            </div>
+                            <div class="forms">
+                                <form method="POST" action="/">
+                                    <input id="terminalInput" type="text" name="terminalInput" placeholder="Input commands here ..."/> <br>
+                                    <input id="publish" type="submit" value="publish">
+                                </form>'
+                            </div>
+                        </div>
+                    </body>
                 </html> '''
 
 #Say handle
 @get('/say')
 def login_form():
-    return '''<form method="POST" action="/say">
-                <input name="say"     type="text" />
-                <input type="submit" />
-              </form>'''
+    return '''  <!doctype html>
+
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>OpenHome</title>
+                        <meta name="description" content="The web interface for your OpenHome!">
+                        <meta name="author" content="OpenHome">
+                        <link rel="stylesheet" href="css/styles.css?v=1.0">
+                    </head>
+
+                    <body>
+                        <script src="index.js"></script>
+                        <div class="body">
+                            <div class="header">
+                                <h2> Welcome to OpenHome </h2>
+                            </div>
+                            <div class="forms">
+                                <form method="POST" action="/say">
+                                    <input name="say" type="text" /> <br>
+                                    <input type="submit" />
+                                </form>'
+                            </div>
+                        </div>
+                    </body>
+                </html> '''
 
 @post('/say') # or @route('/login', method='POST')
 def login_submit():
     say = request.forms.get('say')
     publish.single("hermes/tts/say", json.dumps({"text": say, "siteId": "default"}))
-    return '''<form method="POST" action="/say">
-                <input name="say"     type="text" />
-                <input type="submit" />
-              </form>'''
+
+    return '''  <!doctype html>
+
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>OpenHome</title>
+                        <meta name="description" content="The web interface for your OpenHome!">
+                        <meta name="author" content="OpenHome">
+                        <link rel="stylesheet" href="css/styles.css?v=1.0">
+                    </head>
+
+                    <body>
+                        <script src="index.js"></script>
+                        <div class="body">
+                            <div class="header">
+                                <h2> Welcome to OpenHome </h2>
+                            </div>
+                            <div class="forms">
+                                <form method="POST" action="/say">
+                                    <input name="say" type="text" /> <br>
+                                    <input type="submit" />
+                                </form>'
+                            </div>
+                        </div>
+                    </body>
+                </html> '''
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
