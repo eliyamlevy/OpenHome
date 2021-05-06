fce90bb7fb1a18f2bc41fab50d8cac820ad79a1a
CharliePyle <charliepyle11@gmail.com>
2021-05-06 12:40:26 -0700

    deleted unnecessary test file

--- speech_to_controller_integration/test_payload.py
+++
@@ -1,5 +0,0 @@
-import requests
-string = 'http://127.0.0.1:4151/pub?topic=controller'
-headers = {'data': 'hello world 3'}
-data = '{\"hello\":\"world\"}' 
-res = requests.post(string, data=data, headers=headers)
